a01eb135a0d34544d5a3bd646315280ab39370bf
James Ocampo <124105215+KuyaJimbo@users.noreply.github.com>
2025-05-24 08:56:16 -0400

    Update PlayerJump.cs

--- Unity Projects/3D Adventure/PlayerJump.cs
+++ Unity Projects/3D Adventure/PlayerJump.cs
@@ -4,16 +4,15 @@ using UnityEngine;
 
 public class PlayerJump : MonoBehaviour
 {
+    // Create the VARIABLES:
     // Jump Force (Hint: float)
-    
-
-    // Jump Sound (Hint: AudioClip)
     
 
     // Rigidbody component (Hint: Rigidbody)
     
 
     // Ground check (Hint: bool)
+    
 
     // Start is called before the first frame update
     void Start()
@@ -26,9 +25,9 @@ public class PlayerJump : MonoBehaviour
     void Update()
     {
         // Check if the player is grounded AND the space key is pressed (Hint: Grounded && Input.GetKeyDown(KeyCode.Space))
-
             // AddForce(Vector3.up * jumpForce, ForceMode.Impulse); // Apply a force to the Rigidbody to make it jump
             // Set your ground check to false (Hint: Grounded = false)
+        
         
     }
 
@@ -39,6 +38,7 @@ public class PlayerJump : MonoBehaviour
         if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
         {
             // Set your ground check to true (Hint: Grounded = true)
+            
         }
     }
 }
