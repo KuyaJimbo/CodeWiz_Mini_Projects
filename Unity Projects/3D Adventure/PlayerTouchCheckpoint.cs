626cb910f1fb8cddc5a0487858bdaba5ad57cdc6
James Ocampo <124105215+KuyaJimbo@users.noreply.github.com>
2025-05-30 11:30:18 -0400

    Update PlayerTouchCheckpoint.cs

--- Unity Projects/3D Adventure/PlayerTouchCheckpoint.cs
+++ Unity Projects/3D Adventure/PlayerTouchCheckpoint.cs
@@ -7,24 +7,8 @@ public class PlayerTouchCheckpoint : MonoBehaviour
         if (collision.gameObject.CompareTag("Player"))
         {
             Transform checkpointSpawn = transform.Find("CheckpointSpawn");
-
-            if (checkpointSpawn != null)
-            {
-                TeleportPlayer teleportScript = collision.gameObject.GetComponent<TeleportPlayer>();
-
-                if (teleportScript != null)
-                {
-                    teleportScript.SetCheckpoint(checkpointSpawn);
-                }
-                else
-                {
-                    Debug.LogWarning("TeleportPlayer script not found on Player.");
-                }
-            }
-            else
-            {
-                Debug.LogWarning("CheckpointSpawn child not found on checkpoint object.");
-            }
+            TeleportPlayer teleportScript = collision.gameObject.GetComponent<TeleportPlayer>();
+            teleportScript.SetCheckpoint(checkpointSpawn);
         }
     }
 }
